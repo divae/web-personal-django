29cb1148c3166f531f756ab9afa1239680e61afa
Estela Medrano <stlmedrano@gmail.com>
2019-06-19 12:01:13 +0200

    refactorizadas vistas

--- app/core/tests/test_views.py
+++ app/core/tests/test_views.py
@@ -1,24 +1,26 @@
 from django.test import TestCase
 
+ROOT_TEMPLATE_PATH = 'core/pages/'
+
 
 class TestMainNavigation(TestCase):
 
     def test_home(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/home.html')
+        self.assertTemplateUsed(response, ROOT_TEMPLATE_PATH + 'home.html')
 
     def test_about_me(self):
         response = self.client.get('/about-me/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/about_me.html')
+        self.assertTemplateUsed(response, ROOT_TEMPLATE_PATH + 'about_me.html')
 
     def test_portfolio(self):
         response = self.client.get('/portfolio/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/portfolio.html')
+        self.assertTemplateUsed(response, ROOT_TEMPLATE_PATH + 'portfolio.html')
 
     def test_contact(self):
         response = self.client.get('/contact/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/contact.html')
+        self.assertTemplateUsed(response, ROOT_TEMPLATE_PATH + 'contact.html')
--- app/core/views.py
+++ app/core/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render, HttpResponse
 
+ROOT_TEMPLATE_PATH = 'core/pages/'
+
 
 def home(request):
-    html_response = render(request, 'core/home.html')
+    html_response = render(request, ROOT_TEMPLATE_PATH + 'home.html')
     return HttpResponse(html_response)
 
 
 def about_me(request):
-    html_response = render(request, 'core/about_me.html')
+    html_response = render(request, ROOT_TEMPLATE_PATH + 'about_me.html')
     return HttpResponse(html_response)
 
 
 def portfolio(request):
-    html_response = render(request, 'core/portfolio.html')
+    html_response = render(request, ROOT_TEMPLATE_PATH + 'portfolio.html')
     return HttpResponse(html_response)
 
 
 def contact(request):
-    html_response = render(request, 'core/contact.html')
+    html_response = render(request, ROOT_TEMPLATE_PATH + 'contact.html')
     return HttpResponse(html_response)
